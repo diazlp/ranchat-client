6926b66f92ae43b4ef74658bd75eb7561b606259
Diaz Linggaputra <diazlinggaputra@gmail.com>
2022-05-24 20:59:27 +0700

    added User Id to local storage on user action

--- src/actions/userAction.js
+++ src/actions/userAction.js
@@ -27,6 +27,7 @@ export const getToken = (data) => {
       password: data.password,
     })
     .then(({ data }) => {
+      localStorage.setItem("UserId", data.id);
       localStorage.setItem("access_token", data.access_token);
       localStorage.setItem("email", data.profile?.email);
       localStorage.setItem("fullName", data.profile?.fullName);
