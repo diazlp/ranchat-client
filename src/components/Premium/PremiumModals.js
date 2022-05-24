e335e5ba36d0dd853cdf84ab21ebd2136d5f05f8
Aliansyah Firdaus <aliansyah.it@gmail.com>
2022-05-24 15:26:08 +0700

    final styling modal Premium checkoyt

--- src/components/Premium/PremiumModals.js
+++ src/components/Premium/PremiumModals.js
@@ -1,4 +1,5 @@
-import { Modal, Button } from "react-bootstrap";
+import { Modal, Button, Col, Row, Table } from "react-bootstrap";
+import Icon from "../Icon/Icon";
 
 export default function PremiumModal(props) {
   return (
@@ -7,24 +8,113 @@ export default function PremiumModal(props) {
       size="lg"
       aria-labelledby="contained-modal-title-vcenter"
       centered
+      id="modal-premium"
     >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Modal heading
+      <Modal.Header closeButton className="modal-premium-header">
+        <Modal.Title
+          id="contained-modal-title-vcenter"
+          className="d-flex justify-content-center text-center"
+        >
+          Upgrade to Premium
         </Modal.Title>
       </Modal.Header>
-      <Modal.Body>
-        <h4>Centered Modal</h4>
-        <p>
-          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-          consectetur ac, vestibulum at eros.
-        </p>
+      <Modal.Body className="modal-premium-body d-flex gap-3">
+        <Col className="bg-light col-5 text-center free">
+          <h2 className="mb-4">Free</h2>
+          <div className="table-price mb-5">
+            <Table borderless hover>
+              <tbody>
+                <tr>
+                  <td>
+                    <div className="me-2">
+                      <Icon name="xmark" placement="table-price-exclude" />
+                    </div>
+                  </td>
+                  <td className="text-start">Bisa video call teman</td>
+                </tr>
+                <tr>
+                  <td>
+                    <div className="me-2">
+                      <Icon name="xmark" placement="table-price-exclude" />
+                    </div>
+                  </td>
+                  <td className="text-start">Bisa video call teman</td>
+                </tr>
+                <tr>
+                  <td>
+                    <div className="me-2">
+                      <Icon name="xmark" placement="table-price-exclude" />
+                    </div>
+                  </td>
+                  <td className="text-start">Bisa video call teman</td>
+                </tr>
+                <tr>
+                  <td>
+                    <div className="me-2">
+                      <Icon name="xmark" placement="table-price-exclude" />
+                    </div>
+                  </td>
+                  <td className="text-start">Bisa video call teman</td>
+                </tr>
+              </tbody>
+            </Table>
+          </div>
+          <h5 className="free-footer">Current Package</h5>
+        </Col>
+        <Col className="bg-light text-center premium">
+          <h2 className="mb-4">Premium</h2>
+          <div className="table-price mb-3">
+            <Table borderless hover responsive="sm">
+              <tbody>
+                <tr>
+                  <td>
+                    <div className="me-5">
+                      <Icon name="check" placement="table-price-include" />
+                    </div>
+                  </td>
+                  <td className="text-start">Bisa video call teman</td>
+                </tr>
+                <tr>
+                  <td>
+                    <div className="me-5">
+                      <Icon name="check" placement="table-price-include" />
+                    </div>
+                  </td>
+                  <td className="text-start">Bisa bisa kirim GIF sesukanya</td>
+                </tr>
+                <tr>
+                  <td>
+                    <div className="me-5">
+                      <Icon name="check" placement="table-price-include" />
+                    </div>
+                  </td>
+                  <td className="text-start">Kirim lokasi terkini kamu!</td>
+                </tr>
+                <tr>
+                  <td>
+                    <div className="me-5">
+                      <Icon name="check" placement="table-price-include" />
+                    </div>
+                  </td>
+                  <td className="text-start">Kirim lokasi terkini kamu!</td>
+                </tr>
+              </tbody>
+            </Table>
+          </div>
+          <div className="d-flex justify-content-between px-3 align-items-center premium-footer">
+            <h2 className="text-secondary m-0">Rp. 993rb</h2>
+            <Button
+              onClick={props.onHide}
+              className="btn btn-premium-payment btn-lg"
+            >
+              Premium Now
+            </Button>
+          </div>
+        </Col>
       </Modal.Body>
-      <Modal.Footer>
+      {/* <Modal.Footer className="modal-premium-footer">
         <Button onClick={props.onHide}>Premium Now</Button>
-        {/* <ButtonPrimary text="Premium Now" placement="premium-cta" /> */}
-      </Modal.Footer>
+      </Modal.Footer> */}
     </Modal>
   );
 }
